0255f388f64ff85dec1ec4cc1a9155dd262d4406
Tara Harwood <tara.m.harwood@gmail.com>
2021-04-21 13:37:53 -0700

    basic 3x4 grid

--- src/board.js
+++ src/board.js
@@ -26,22 +26,22 @@ class Board extends React.Component {
     }
   };
 
-  isActive(id) {
-    if (!this.props.isActive) return false;
-    if (this.props.G.cells[id] !== null) return false;
-    return true;
-  }
+  // isActive(id) {
+  //   if (!this.props.isActive) return false;
+  //   if (this.props.G.cells[id] !== null) return false;
+  //   return true;
+  // }
 
   render() {
     let tbody = [];
     for (let i = 0; i < 3; i++) {
       let cells = [];
-      for (let j = 0; j < 3; j++) {
+      for (let j = 0; j < 4; j++) {
         const id = 3 * i + j;
         cells.push(
           <td
             key={id}
-            className={this.isActive(id) ? 'active' : ''}
+            // className={this.isActive(id) ? 'active' : ''}
             onClick={() => this.onClick(id)}
           >
             {this.props.G.cells[id]}
@@ -51,22 +51,22 @@ class Board extends React.Component {
       tbody.push(<tr key={i}>{cells}</tr>);
     }
 
-    let winner = null;
-    if (this.props.ctx.gameover) {
-      winner =
-        this.props.ctx.gameover.winner !== undefined ? (
-          <div id="winner">Winner: {this.props.ctx.gameover.winner}</div>
-        ) : (
-            <div id="winner">Draw!</div>
-          );
-    }
+    // let winner = null;
+    // if (this.props.ctx.gameover) {
+    //   winner =
+    //     this.props.ctx.gameover.winner !== undefined ? (
+    //       <div id="winner">Winner: {this.props.ctx.gameover.winner}</div>
+    //     ) : (
+    //         <div id="winner">Draw!</div>
+    //       );
+    // }
 
     return (
       <div>
         <table id="board">
           <tbody>{tbody}</tbody>
         </table>
-        {winner}
+        {/* {winner} */}
       </div>
     );
   }
--- src/game.js
+++ src/game.js
@@ -6,31 +6,31 @@
  * https://opensource.org/licenses/MIT.
  */
 
-function IsVictory(cells) {
-  const positions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
+// function IsVictory(cells) {
+//   const positions = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6]
+//   ];
 
-  const isRowComplete = row => {
-    const symbols = row.map(i => cells[i]);
-    return symbols.every(i => i !== null && i === symbols[0]);
-  };
+//   const isRowComplete = row => {
+//     const symbols = row.map(i => cells[i]);
+//     return symbols.every(i => i !== null && i === symbols[0]);
+//   };
 
-  return positions.map(isRowComplete).some(i => i === true);
-}
+//   return positions.map(isRowComplete).some(i => i === true);
+// }
 
 const TicTacToe = {
   name: "tic-tac-toe",
 
   setup: () => ({
-    cells: Array(9).fill(null)
+    cells: Array(12).fill(null)
   }),
 
   moves: {
@@ -41,28 +41,28 @@ const TicTacToe = {
     }
   },
 
-  turn: { moveLimit: 1 },
+  // turn: { moveLimit: 1 },
 
-  endIf: (G, ctx) => {
-    if (IsVictory(G.cells)) {
-      return { winner: ctx.currentPlayer };
-    }
-    if (G.cells.filter(c => c === null).length === 0) {
-      return { draw: true };
-    }
-  },
+  // endIf: (G, ctx) => {
+  //   if (IsVictory(G.cells)) {
+  //     return { winner: ctx.currentPlayer };
+  //   }
+  //   if (G.cells.filter(c => c === null).length === 0) {
+  //     return { draw: true };
+  //   }
+  // },
 
-  ai: {
-    enumerate: G => {
-      let moves = [];
-      for (let i = 0; i < 9; i++) {
-        if (G.cells[i] === null) {
-          moves.push({ move: "clickCell", args: [i] });
-        }
-      }
-      return moves;
-    }
-  }
+  // ai: {
+  //   enumerate: G => {
+  //     let moves = [];
+  //     for (let i = 0; i < 9; i++) {
+  //       if (G.cells[i] === null) {
+  //         moves.push({ move: "clickCell", args: [i] });
+  //       }
+  //     }
+  //     return moves;
+  //   }
+  // }
 };
 
 export default TicTacToe;
